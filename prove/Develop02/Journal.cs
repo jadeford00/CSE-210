82dd8da9be38bdac8010bfbc84432bf2f2812b5e
jadeford00 <for20004@byui.edu>
2023-02-21 18:22:51 -0700

    Assignment Done

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,46 @@
+using System.IO;
+
+public class Journal
+{
+    public List<string> _entriesList = new List<string>();
+
+
+    public void Display()
+    {
+        foreach (string entry in _entriesList)
+        {
+            Console.WriteLine($"{entry}");
+        }
+    }
+
+
+    public void Load()
+    {
+        Console.WriteLine("What is the file name? ");
+        string _fileName = Console.ReadLine();
+        string[] lines = System.IO.File.ReadAllLines(_fileName);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+            string _loadEntry = parts[0];
+            _entriesList.Add(_loadEntry);
+        }
+
+    }
+
+
+    public void Save()
+    {
+        Console.WriteLine("What would you like to name the file? (Please follow by adding '.txt' to the end)");
+        string _fileName = Console.ReadLine();
+
+        using (StreamWriter outputFile = new StreamWriter(_fileName))
+        {
+             foreach (string entry in _entriesList)
+             {
+                outputFile.WriteLine($"{entry}");
+             }
+        }
+    }
+}
