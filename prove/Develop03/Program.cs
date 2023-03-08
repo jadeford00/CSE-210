159b5ba657ce18352a890e4b1098e3066791b572
jadeford00 <for20004@byui.edu>
2023-03-08 15:45:56 -0700

    Assignment Done

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,41 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Book book = new Book();
+
+        Console.WriteLine("Welcome to the Scripture Memorizer\n");
+        Console.WriteLine("Enter '1' to choose a random verse.");
+        Console.WriteLine("Enter '2' to choose your own verse. ");
+        string input = Console.ReadLine();
+
+
+        if (input == "1")
+        {
+            book.ChooseScripture();
+        }
+        else
+        {
+            book.InputNewScripture();
+        }
+
+        Scripture scripture = new Scripture(book.GetScripture());
+
+        scripture.DisplayScripture();
+        Console.WriteLine("\n\nPress enter to hide words or input 'quit' to exit. ");
+        input = Console.ReadLine();
+        
+        while (input != "quit")
+        {
+            Console.Clear();
+            scripture.HideWords();
+            scripture.DisplayScripture();
+
+            Console.WriteLine("\n\nPress enter to hide words ot input 'quit' to exit. ");
+            input = Console.ReadLine();
+        }
     }
 }
